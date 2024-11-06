4d4add0122055db25838a1576974ee3912dea61d
Jerry Janvier <jerrythefilmmaker@gmail.com>
2024-11-06 13:22:32 -0500

    added site primary color in tailwin.config file

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,20 +8,20 @@ const Navbar = () => {
         <img className='w-44 cursor-pointer' src={assets.logo} alt="logo" />
         <ul className='hidden md:flex items-start gap-5 font-medium'>
             <NavLink>
-                <li>Home</li>
-                <hr />
+                <li className='py-1'>Home</li>
+                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto'/>
             </NavLink>
             <NavLink>
-                <li>All Doctors</li>
-                <hr />
+                <li className='py-1'>All Doctors</li>
+                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto '/>
             </NavLink>
             <NavLink>
-                <li>About</li>
-                <hr />
+                <li className='py-1'>About</li>
+                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto '/>
             </NavLink>
             <NavLink>
-                <li>Contact</li>
-                <hr />
+                <li className='py-1'>Contact</li>
+                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto '/>
             </NavLink>
         </ul>
     </div>
